6cb01bd4050d8c638995a68a60b99b34e00dc2ea
PlotvaIzLodzya <gordoy92@gmail.com>
2025-06-20 21:33:42 +0300

    Rework to proper account for linear damping

--- Assets/Scripts/CharacterControll.cs
+++ Assets/Scripts/CharacterControll.cs
@@ -11,6 +11,7 @@ public class CharacterControl : MonoBehaviour
     [SerializeField] private ContactFilter2D _contactFilter;
     
     private bool _isOnSteepSlope;
+    private float _elapsedTime;
     private ContactPoint2D[] _points;
     private Vector2 _direction;
     private Rigidbody2D _rb;
@@ -28,11 +29,18 @@ public class CharacterControl : MonoBehaviour
             _direction += Vector2.left;
         if (Input.GetKey(KeyCode.D))
             _direction += Vector2.right;
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _elapsedTime = 0;
+        }
     }
 
     private void FixedUpdate()
     {
-        var forceMagnitude = _rb.mass * (_maxSpeed / _accelerationTime + _rb.linearDamping * _maxSpeed);
+        var exponent = Mathf.Exp(-_rb.linearDamping/_rb.mass * _accelerationTime);
+        var denominator = 1f - exponent;
+        var forceMagnitude = _maxSpeed * (_rb.mass * _rb.linearDamping) / denominator;
         
         var normal = GetNormal();
         var direction = GetDirectionAlongSurface(_direction, normal);
@@ -40,9 +48,14 @@ public class CharacterControl : MonoBehaviour
         
         var force = direction * forceMagnitude + slopeCounterForce;
         Debug.DrawRay(_rb.position, force, Color.red);
-        _rb.AddForce(force);
         
+        _rb.AddForce(force);
         _rb.linearVelocityX = Mathf.Clamp(_rb.linearVelocityX, -_maxSpeed, _maxSpeed);
+        if (force.magnitude > 0 && _elapsedTime > _accelerationTime)
+        {
+            Debug.Log($"time: {_elapsedTime} speed: {_rb.linearVelocity.magnitude}");
+        }
+        _elapsedTime += Time.fixedDeltaTime;
     }
 
     private Vector2 GetDirectionAlongSurface(Vector2 direction, Vector2 normal)
@@ -90,7 +103,7 @@ public class CharacterControl : MonoBehaviour
         return force;
     }
     
-    public bool IsSameDirection(Vector3 vectorA, Vector3 vectorB)
+    private bool IsSameDirection(Vector3 vectorA, Vector3 vectorB)
     {
         return Vector3.Angle(vectorA, vectorB) < 90;
     }
